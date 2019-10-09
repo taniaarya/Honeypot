e8e478f12f5baf0e3f6f6ca2a0036c2ba844aceb
root <root@proxmox.aces.umd.edu>
2019-10-09 09:49:52 -0400

    added RAM to health check

--- check_health.sh
+++ check_health.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 pdisk=$(/bin/df -BM --total | /bin/grep total | /usr/bin/awk '{print $4}'| sed 's/.$//')
+pram=$(/usr/bin/free --mega | /bin/grep "Mem" | /usr/bin/awk '{print $7}')
 
 if [ $pdisk -le 1000 ]
 then
-	echo "here"
-	/usr/local/bin/python3.6 /root/Honeypot_Scripts/send_health_update.py $pdisk
+	/usr/local/bin/python3.6 /root/Honeypot_Scripts/send_health_update.py $pdisk $pram
+elif [ $pram -le 1000 ]
+then
+	/usr/local/bin/python3.6 /root/Honeypot_Scripts/send_health_update.py $pdisk $pram
 fi
 exit 0
--- send_health_update.py
+++ send_health_update.py
@@ -3,11 +3,13 @@ import os
 import slack
 
 disk_space = sys.argv[1]
+ram = sys.argv[2]
 
 TOKEN = os.getenv('SLACK_TOKEN')
 slack_client = slack.WebClient(token=TOKEN)
 
 message = ":warning::warning: The Honeypot is SICK :warning::warning:\n"
 message += "The disk space is at {}MB\n".format(disk_space)
+message += "The RAM is at {}MB\n".format(ram)
 response = slack_client.chat_postMessage(channel='#2c_attackers', text=message, username="Health Update")
 
