8a0ddfd8c0b345dc9007e305cdae8d79565ffeff
root <root@proxmox.aces.umd.edu>
2019-10-09 01:02:12 -0400

    fixed bugs

--- data_collection.py
+++ data_collection.py
@@ -77,6 +77,7 @@ with gzip.open(filepath, "rt", encoding="utf-8") as file:
       # sets up new command mailing
   if mail_new_command != "":
     message = ":bookmark::bookmark: Y'all be slacking on that dictionary :bookmark::bookmark:\n\t"
+    message += "New Commands Found: \n\t"
     message += mail_new_command + "\n\t"
     try:
       response = slack_client.chat_postMessage(channel='#2c_attackers', text=message, username="New Command Found")
