6a698f090b1ecddab46b014a3fc96b53e820b8fd
root <root@proxmox.aces.umd.edu>
2019-10-10 09:58:46 -0400

    added mentions

--- send_health_update.py
+++ send_health_update.py
@@ -11,6 +11,6 @@ slack_client = slack.WebClient(token=TOKEN)
 message = ":warning::warning: The Honeypot is SICK :warning::warning:\n"
 message += "The disk space is at {}MB\n".format(disk_space)
 message += "The RAM is at {}MB\n".format(ram)
-message += "<@Tania Arya> <@Ethan K> <@Irene Li> <@Nicole Lenhoff>"
-response = slack_client.chat_postMessage(channel='#2c_attackers', link_names=True, text=message, username="Health Update")
+message += "<@UN4ME1ARL> <@UN21E47PW> <@UMPMMH3PU> <@UMPMMGQ82>"
+response = slack_client.chat_postMessage(channel='#2c_attackers', text=message, username="Health Update")
 
