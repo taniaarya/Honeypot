0a9d4251324b05951714d59853082f10a0581af4
taniaarya <tania.arya@gmail.com>
2019-10-10 00:50:01 -0400

    added last names

--- send_health_update.py
+++ send_health_update.py
@@ -11,6 +11,6 @@ slack_client = slack.WebClient(token=TOKEN)
 message = ":warning::warning: The Honeypot is SICK :warning::warning:\n"
 message += "The disk space is at {}MB\n".format(disk_space)
 message += "The RAM is at {}MB\n".format(ram)
-message += "@Tania @Ethan @Irene @Nicole"
+message += "@Tania Arya @Ethan K @Irene Li @Nicole Lenhoff"
 response = slack_client.chat_postMessage(channel='#2c_attackers', link_names=4, text=message, username="Health Update")
 
