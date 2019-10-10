ade78eec0f6a5f148f7f6a6fd47c654d12efba9b
taniaarya <tania.arya@gmail.com>
2019-10-10 00:52:46 -0400

    link names true

--- send_health_update.py
+++ send_health_update.py
@@ -12,5 +12,5 @@ message = ":warning::warning: The Honeypot is SICK :warning::warning:\n"
 message += "The disk space is at {}MB\n".format(disk_space)
 message += "The RAM is at {}MB\n".format(ram)
 message += "<@Tania Arya> <@Ethan K> <@Irene Li> <@Nicole Lenhoff>"
-response = slack_client.chat_postMessage(channel='#2c_attackers', link_names=4, text=message, username="Health Update")
+response = slack_client.chat_postMessage(channel='#2c_attackers', link_names=True, text=message, username="Health Update")
 
