e82faff49a4cff9ced0c125d8169cfb4b337667f
taniaarya <tania.arya@gmail.com>
2019-10-12 01:14:50 -0400

    added print to timer

--- timer.sh
+++ timer.sh
@@ -19,9 +19,11 @@ pkill -f "tail -n 0 -F /var/lib/lxc/$1/rootfs/var/log/auth.log"
 disConnTime=$(date +%H:%M:%S)
 
 # calls recycling script passing ctid, ctip, and mitm port
+echo "calling recyling /root/Honeypot_Scripts/recycling_script.sh $1 $2 $3 $6"
 /root/Honeypot_Scripts/recycling_script.sh $1 $2 $3 $6 &
 
 # calls data collection script with session id and filesystem, ctid, attacker ip
+echo "calling data_collection /root/Honeypot_Scripts/call_data_collection.sh $4 $5 $1 $6 $disConnTime"
 /root/Honeypot_Scripts/call_data_collection.sh $4 $5 $1 $6 $disConnTime &
 
 # makes sure disk space is good
