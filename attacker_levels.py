3e30f935304c66c5cc1200aef308a89629aa3ee6
taniaarya <tania.arya@gmail.com>
2019-10-13 11:45:48 -0400

    added touch

--- attacker_levels.py
+++ attacker_levels.py
@@ -1,5 +1,5 @@
 attacker_levels = {"exit": 1, "logout": 1,
                    "ls": 2, "cat": 2, "cd": 2, "less": 2, "more": 2, 
-                   "ps": 2, "grep": 2, "ifconfig": 2, "uptime": 2, "uname": 2, "echo": 2, "pwd": 2, "cwd": 2, "du":2, "df":2, "who":2, "whoami": 2,  "find":2, "top":2, "ps":2, "jobs":2,"date":2, "cal":2, "diff":2, "man":2, "help":2, "ssh":2, "wc":2, "gunzip":2, "zcat":2, "basename":2, "bc":2, "dd":2,
+                   "ps": 2, "grep": 2, "ifconfig": 2, "uptime": 2, "uname": 2, "echo": 2, "pwd": 2, "cwd": 2, "du":2, "df":2, "who":2, "whoami": 2,  "find":2, "top":2, "ps":2, "jobs":2,"date":2, "cal":2, "diff":2, "man":2, "help":2, "ssh":2, "wc":2, "gunzip":2, "zcat":2, "basename":2, "bc":2, "dd":2, "touch":2,
                    "cp": 3, "mv": 3, "export": 3, "scp": 3, "irb":2, "cksum":2, "cmp":2, "comm":2, "csplit":2, "dirname":2,
                    "mkdir": 3, "rmdir": 3, "nano": 3, "pico": 3, "vim": 3, "vi": 3, "rm": 3, "import": 3, "chmod": 3, "get": 3, "wget": 3, "curl": 3, "tar": 3, "gzip": 3, "awk": 3, "ftp": 3, "kill":3, "alias":3, "ar":3, "ruby":3, "bg":3, "fg" :3, "cc": 3, "c99":3, "gcc":3, "chgrp":3, "crontab":3, "cut":3, "env":3}
