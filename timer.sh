b12414bc5ab3cc80deb49fc655e7ba023d023b50
taniaarya <tania.arya@gmail.com>
2019-11-02 12:18:27 -0400

    changed timer

--- timer.sh
+++ timer.sh
@@ -9,7 +9,7 @@
 # $7 = timestamp
 
 # kicks attacker out after 30 minutes
-sleep 1800
+sleep 10
 
 # time attacker is kicked out
 disConnTime=$(date +%H:%M:%S)
@@ -24,18 +24,18 @@ echo "killing node /root/MITM/mitm/index.js HACS200_2C $3 $2 $1 true mitm.js"
 pkill -f "node /root/MITM/mitm/index.js HACS200_2C $3 $2 $1 true mitm.js"
 
 # allows mitm process to be fully killed before adding firewall rules
-sleep 10
+sleep 25
 
 #ps_aux=$(ps aux | grep node)
 #echo "$ps_aux"
 
 # calls recycling script passing ctid, ctip, and mitm port
 echo "calling recyling /root/Honeypot_Scripts/recycling_script.sh $1 $2 $3 $6"
-/root/Honeypot_Scripts/recycling_script.sh $1 $2 $3 $6 $7 >> /root/Logs/recycling/$timestamp>&1 &
+/root/Honeypot_Scripts/recycling_script.sh $1 $2 $3 $6 $7 > /root/Logs/recycling/$7>&1 &
 
 # calls data collection script with session id and filesystem, ctid, attacker ip
 echo "calling data_collection /root/Honeypot_Scripts/call_data_collection.sh $4 $5 $1 $6 $disConnTime"
-/root/Honeypot_Scripts/call_data_collection.sh $4 $5 $1 $6 $disConnTime >> /root/Logs/data_collection/$7>&1 &
+/root/Honeypot_Scripts/call_data_collection.sh $4 $5 $1 $6 $disConnTime > /root/Logs/data_collection/$7>&1 &
 
 # makes sure disk space is good
 /root/Honeypot_Scripts/check_health.sh &
