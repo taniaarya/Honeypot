9a66dbd705fc6ae77fea95f1de82bb449e928ea1
taniaarya <tania.arya@gmail.com>
2019-11-02 01:08:24 -0400

    fixed redirection

--- reset_test.sh
+++ reset_test.sh
@@ -6,12 +6,14 @@ pkill data_coll
 pkill timer
 pkill recy
 
+timestamp=$(date +%Y-%m-%d-%H:%M:%S)
+
 /root/Honeypot_Project/firewall/firewall_rules.sh
 
-nohup node /root/MITM/mitm/index.js HACS200_2C 10000 172.20.0.2 101 true mitm.js > /root/Logs/mitm_file101>&1 &
-nohup node /root/MITM/mitm/index.js HACS200_2C 10001 172.20.0.3 102 true mitm.js > /root/Logs/mitm_file102>&1 &
-nohup node /root/MITM/mitm/index.js HACS200_2C 10002 172.20.0.4 103 true mitm.js > /root/Logs/mitm_file103>&1 &
-nohup node /root/MITM/mitm/index.js HACS200_2C 10003 172.20.0.5 104 true mitm.js > /root/Logs/mitm_file104>&1 &
+nohup node /root/MITM/mitm/index.js HACS200_2C 10000 172.20.0.2 101 true mitm.js > /root/Logs/MITM_101/$timestamp>&1 &
+nohup node /root/MITM/mitm/index.js HACS200_2C 10001 172.20.0.3 102 true mitm.js > /root/Logs/MITM_102/$timestamp>&1 &
+nohup node /root/MITM/mitm/index.js HACS200_2C 10002 172.20.0.4 103 true mitm.js > /root/Logs/MITM_103/$timestamp>&1 &
+nohup node /root/MITM/mitm/index.js HACS200_2C 10003 172.20.0.5 104 true mitm.js > /root/Logs/MITM_104/$timestamp>&1 &
 
 nohup /root/Honeypot_Scripts/tailing_script.sh 101 172.20.0.2 "Yes" 10000 > /root/Logs/tail101>&1 &
 nohup /root/Honeypot_Scripts/tailing_script.sh 102 172.20.0.3 "Yes" 10001 > /root/Logs/tail102>&1 &
