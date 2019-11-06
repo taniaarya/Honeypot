3c1796ac6c364a3901c18ea1a7bc9047d1ff507a
taniaarya <tania.arya@gmail.com>
2019-11-05 20:51:23 -0500

    added sleep

--- reset_test.sh
+++ reset_test.sh
@@ -10,6 +10,8 @@ timestamp=$(date +%Y-%m-%d-%H:%M:%S)
 
 /root/Honeypot_Project/firewall/firewall_rules.sh
 
+sleep 10
+
 nohup node /root/MITM/mitm/index.js HACS200_2C 10000 172.20.0.2 101 true mitm.js > /root/Logs/MITM_101/$timestamp>&1 &
 nohup node /root/MITM/mitm/index.js HACS200_2C 10001 172.20.0.3 102 true mitm.js > /root/Logs/MITM_102/$timestamp>&1 &
 nohup node /root/MITM/mitm/index.js HACS200_2C 10002 172.20.0.4 103 true mitm.js > /root/Logs/MITM_103/$timestamp>&1 &
